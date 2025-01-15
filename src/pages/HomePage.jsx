e413ac3da1cadf9fc77a055b5f1aa838b3b1b62b
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2025-01-15 05:51:59 +0100

    feat: import hero section and add grid layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,26 @@
 import Header from '@/components/Header';
+import HeroSection from '@/components/HeroSection';
+import styled from 'styled-components';
+
+const HomePageWrapper = styled.div`
+  & {
+    min-height: 100dvh;
+    display: grid;
+    grid-template-rows: 45px 1fr;
+  }
+
+  @media screen and (min-width: 1024px) {
+    & {
+      grid-template-rows: 127px 1fr;
+    }
+  }
+`;
 
 export default function HomePage() {
-  return <Header />;
+  return (
+    <HomePageWrapper>
+      <Header />
+      <HeroSection />
+    </HomePageWrapper>
+  );
 }
