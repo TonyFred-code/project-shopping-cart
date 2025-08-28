2ca5fcdf0e6fa86f16969eefeef7f0d47a020d0f
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-08-28 19:41:18 +0100

    feat: add fruits fetching through useFruitsData
    add loader comp

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,9 @@ import HeroSection from '@/components/HeroSection.jsx';
 import SideMenu from '@/components/SideMenu.jsx';
 import OnSaleSection from '@/components/OnSaleSection.jsx';
 import InSeasonSection from '@/components/InSeasonSection.jsx';
+import { ThreeDots } from 'react-loader-spinner';
+import useFruitsData from '@/helpers/useFruitsData.jsx';
+import { fruitOnSale } from '@/helpers/fruits-helper.js';
 
 const HomePageWrapper = styled.div`
   & {
@@ -16,17 +19,40 @@ const HomePageWrapper = styled.div`
 
 export default function HomePage() {
   const [sideMenuOpen, setSideMenuOpen] = useState(false);
+  const { error, loading, fruits } = useFruitsData();
+
+  const onSaleFruits = fruits.filter((d) => {
+    return fruitOnSale(d);
+  });
 
   function toggleSideMenuOpen() {
     setSideMenuOpen(!sideMenuOpen);
   }
+
+  // ! REFACTOR FAILED FETCH
+  if (error) return <div> FAILED TO FETCH</div>;
 
   return (
     <HomePageWrapper>
       <Header toggleSideMenuOpen={toggleSideMenuOpen} />
       <SideMenu open={sideMenuOpen} toggleOpen={toggleSideMenuOpen} />
       <HeroSection />
-      <OnSaleSection />
+      {loading ? (
+        <div className="loading-container">
+          <ThreeDots
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            radius="9"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
+      ) : (
+        <OnSaleSection fruits={onSaleFruits} />
+      )}
       <InSeasonSection />
     </HomePageWrapper>
   );
