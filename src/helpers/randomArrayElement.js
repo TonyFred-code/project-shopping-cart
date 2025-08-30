c101fd0b64ccfb69803a55a053e0a96e8ba5e3d6
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-08-30 09:00:35 +0100

    feat: extend function to pass test specs

---
+++ src/helpers/randomArrayElement.js
@@ -0,0 +1,12 @@
+import randomInteger from 'random-int';
+
+export default function randomArrayElement(array) {
+  if (!Array.isArray(array)) throw new TypeError('Excepted type to be array');
+  const arrayLength = array.length;
+
+  if (arrayLength === 0) return null;
+
+  const randomIndex = randomInteger(0, array.length - 1);
+
+  return array[randomIndex];
+}
