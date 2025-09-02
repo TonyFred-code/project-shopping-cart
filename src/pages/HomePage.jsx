53ae5a5f66cf768602690ba12dfbaa92ba062cd0
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-02 06:52:57 +0100

    feat: use LayoutWrapper for repeated page comps

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import Header from '@/components/Header.jsx';
 import HeroSection from '@/components/HeroSection.jsx';
-import SideMenu from '@/components/SideMenu.jsx';
 import OnSaleSection from '@/components/OnSaleSection.jsx';
 import InSeasonSection from '@/components/InSeasonSection.jsx';
 import useFruitsData from '@/helpers/useFruitsData.jsx';
 import { fruitInSeason, fruitOnSale } from '@/helpers/fruits-helper.js';
 import useCategoriesData from '@/helpers/useCategoriesData.jsx';
-import Footer from '@/components/Footer.jsx';
+import ShopByCategory from '@/components/ShopByCategory.jsx';
+import LayoutWrapper from '@/components/Layout.jsx';
+// import SearchMenu from '@/components/SearchMenu.jsx';
 
 const HomePageWrapper = styled.div`
   & {
@@ -19,7 +19,7 @@ const HomePageWrapper = styled.div`
 `;
 
 export default function HomePage() {
-  const [sideMenuOpen, setSideMenuOpen] = useState(false);
+  // const [searchMenuOpen, setSearchMenuOpen] = useState(false);
   const fruitsData = useFruitsData();
   const categoriesData = useCategoriesData();
 
@@ -31,31 +31,24 @@ export default function HomePage() {
     return fruitInSeason(d);
   });
 
-  function toggleSideMenuOpen() {
-    setSideMenuOpen(!sideMenuOpen);
-  }
-
   // ! REFACTOR FAILED FETCH
-  if (fruitsData.error) return <div> FAILED TO FETCH</div>;
+  if (fruitsData.error || categoriesData.error)
+    return <div> FAILED TO FETCH</div>;
 
   return (
     <HomePageWrapper>
-      <Header
-        toggleSideMenuOpen={toggleSideMenuOpen}
-        categories={categoriesData.categories}
-      />
-      <SideMenu
-        open={sideMenuOpen}
-        toggleOpen={toggleSideMenuOpen}
-        categories={categoriesData.categories}
-      />
-      <HeroSection />
-      {/*TODO: create about us mini section */}
-      <OnSaleSection fruits={onSaleFruits} loading={fruitsData.loading} />
-      <InSeasonSection fruits={inSeasonFruits} loading={fruitsData.loading} />
-      {/*TODO: create a shop by categories section */}
-      {/*TODO: create footer section */}
-      <Footer />
+      <LayoutWrapper>
+        {/* TODO: turn search menu to its own page */}
+        {/* <SearchMenu open={searchMenuOpen} toggleOpen={toggleSearchMenuOpen} /> */}
+        <HeroSection />
+        <OnSaleSection fruits={onSaleFruits} loading={fruitsData.loading} />
+        <InSeasonSection fruits={inSeasonFruits} loading={fruitsData.loading} />
+        {/*TODO: create a shop by categories section */}
+        <ShopByCategory
+          categories={categoriesData.categories}
+          loading={categoriesData.loading}
+        />
+      </LayoutWrapper>
     </HomePageWrapper>
   );
 }
