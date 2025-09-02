c4f968b7d4926a094d8be43042fea00044c3126b
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-02 06:51:42 +0100

    feat: add Layout comps for each page

---
+++ src/components/Layout.jsx
@@ -0,0 +1,45 @@
+import useCategoriesData from '@/helpers/useCategoriesData.jsx';
+import { useState } from 'react';
+import styled from 'styled-components';
+import Header from './Header.jsx';
+import SideMenu from './SideMenu.jsx';
+import Footer from './Footer.jsx';
+import { node } from 'prop-types';
+
+const Wrapper = styled.div`
+  & {
+    min-height: 100dvh;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export default function LayoutWrapper({ children }) {
+  const [sideMenuOpen, setSideMenuOpen] = useState(false);
+  const categoriesData = useCategoriesData();
+
+  function toggleSideMenuOpen() {
+    setSideMenuOpen(!sideMenuOpen);
+  }
+
+  return (
+    <Wrapper>
+      <Header
+        toggleSideMenuOpen={toggleSideMenuOpen}
+        // toggleSearchMenuOpen={toggleSearchMenuOpen}
+        categories={categoriesData.categories}
+      />
+      <main style={{ flex: 1 }}>{children}</main>
+      <SideMenu
+        open={sideMenuOpen}
+        toggleOpen={toggleSideMenuOpen}
+        categories={categoriesData.categories}
+      />
+      <Footer />
+    </Wrapper>
+  );
+}
+
+LayoutWrapper.propTypes = {
+  children: node.isRequired,
+};
