acaabb36f2e6304ecaae736568b2c4c4589c4a2f
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-02 10:14:53 +0100

    feat: Add smooth scrolling to anchor links
    Implements smooth scrolling functionality for anchor links on the homepage.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import HeroSection from '@/components/HeroSection.jsx';
 import OnSaleSection from '@/components/OnSaleSection.jsx';
@@ -8,6 +8,7 @@ import { fruitInSeason, fruitOnSale } from '@/helpers/fruits-helper.js';
 import useCategoriesData from '@/helpers/useCategoriesData.jsx';
 import ShopByCategory from '@/components/ShopByCategory.jsx';
 import LayoutWrapper from '@/components/Layout.jsx';
+import { useLocation } from 'react-router-dom';
 // import SearchMenu from '@/components/SearchMenu.jsx';
 
 const HomePageWrapper = styled.div`
@@ -20,6 +21,7 @@ const HomePageWrapper = styled.div`
 
 export default function HomePage() {
   // const [searchMenuOpen, setSearchMenuOpen] = useState(false);
+  const location = useLocation();
   const fruitsData = useFruitsData();
   const categoriesData = useCategoriesData();
 
@@ -30,6 +32,15 @@ export default function HomePage() {
   const inSeasonFruits = fruitsData.fruits.filter((d) => {
     return fruitInSeason(d);
   });
+
+  useEffect(() => {
+    if (location.hash) {
+      const el = document.getElementById(location.hash.replace('#', ''));
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location]);
 
   // ! REFACTOR FAILED FETCH
   if (fruitsData.error || categoriesData.error)
