b300ab978c4eb651eec9cc019474d725dbb536c8
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-03 16:47:19 +0100

    feat: remove shop by categories section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import HeroSection from '@/components/HeroSection.jsx';
 import OnSaleSection from '@/components/OnSaleSection.jsx';
 import InSeasonSection from '@/components/InSeasonSection.jsx';
 import useFruitsData from '@/helpers/useFruitsData.jsx';
 import { fruitInSeason, fruitOnSale } from '@/helpers/fruits-helper.js';
-import useCategoriesData from '@/helpers/useCategoriesData.jsx';
-import ShopByCategory from '@/components/ShopByCategory.jsx';
 import LayoutWrapper from '@/components/Layout.jsx';
 import { useLocation } from 'react-router-dom';
 // import SearchMenu from '@/components/SearchMenu.jsx';
@@ -20,10 +18,8 @@ const HomePageWrapper = styled.div`
 `;
 
 export default function HomePage() {
-  // const [searchMenuOpen, setSearchMenuOpen] = useState(false);
   const location = useLocation();
   const fruitsData = useFruitsData();
-  const categoriesData = useCategoriesData();
 
   const onSaleFruits = fruitsData.fruits.filter((d) => {
     return fruitOnSale(d);
@@ -43,22 +39,14 @@ export default function HomePage() {
   }, [location]);
 
   // ! REFACTOR FAILED FETCH
-  if (fruitsData.error || categoriesData.error)
-    return <div> FAILED TO FETCH</div>;
+  if (fruitsData.error) return <div> FAILED TO FETCH</div>;
 
   return (
     <HomePageWrapper>
       <LayoutWrapper>
-        {/* TODO: turn search menu to its own page */}
-        {/* <SearchMenu open={searchMenuOpen} toggleOpen={toggleSearchMenuOpen} /> */}
         <HeroSection />
         <OnSaleSection fruits={onSaleFruits} loading={fruitsData.loading} />
         <InSeasonSection fruits={inSeasonFruits} loading={fruitsData.loading} />
-        {/*TODO: create a shop by categories section */}
-        <ShopByCategory
-          categories={categoriesData.categories}
-          loading={categoriesData.loading}
-        />
       </LayoutWrapper>
     </HomePageWrapper>
   );
