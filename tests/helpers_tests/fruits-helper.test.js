60ac50860e05dbf6fd6ddfcc43d3e09838a92add
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-21 08:08:38 +0100

    test: add test suite for getRandomDiscount

--- tests/helpers_tests/fruits-helper.test.js
+++ tests/helpers_tests/fruits-helper.test.js
@@ -1,5 +1,11 @@
 import { it, expect, describe, afterEach, vi } from 'vitest';
-import { getCurrentMonth } from '../../src/helpers/fruits-helper.js';
+import {
+  getCurrentMonth,
+  getRandomDiscount,
+} from '../../src/helpers/fruits-helper.js';
+
+vi.mock('random-int', { spy: true });
+import randomInteger from 'random-int';
 
 describe('FruitsHelper', () => {
   describe('getCurrentMonth', () => {
@@ -14,6 +20,26 @@ describe('FruitsHelper', () => {
       const currentMonth = getCurrentMonth();
 
       expect(currentMonth).toMatch(/december/i);
+    });
+  });
+
+  describe('getRandomDiscount', () => {
+    it('should use default range values if not args is given', () => {
+      vi.mocked(randomInteger).mockReturnValue(42);
+
+      const discount = getRandomDiscount();
+
+      expect(randomInteger).toHaveBeenCalledWith(5, 55);
+      expect(discount).toBe(42);
+    });
+
+    it('should use custom values if args are given', () => {
+      vi.mocked(randomInteger).mockReturnValue(42);
+
+      const discount = getRandomDiscount(2, 25);
+
+      expect(randomInteger).toHaveBeenCalledWith(2, 25);
+      expect(discount).toBe(42);
     });
   });
 });
