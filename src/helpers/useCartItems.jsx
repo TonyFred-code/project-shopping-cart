8389664abe35a308ebe97a2cb3aab84857133c78
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-28 13:01:57 +0100

    refactor: use fetchCartItems in custom hook

--- src/helpers/useCartItems.jsx
+++ src/helpers/useCartItems.jsx
@@ -1,28 +1,14 @@
-import randomInteger from 'random-int';
 import { useEffect, useState } from 'react';
+import fetchCartItems from './fetchCartItems.js';
 
 export default function useCartItems() {
   const [loading, setLoading] = useState(true);
   const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    const CART_KEY = 'cart_items';
-    const LOADER_DURATION = randomInteger(200, 500);
-
-    function loadCartItems() {
-      const data = JSON.parse(localStorage.getItem(CART_KEY));
-
-      if (data && data.length > 0) {
-        return data;
-      }
-
-      return [];
-    }
-
-    setCartItems(loadCartItems());
-    setTimeout(() => {
-      setLoading(false);
-    }, LOADER_DURATION);
+    fetchCartItems()
+      .then((data) => setCartItems(data))
+      .finally(() => setLoading(false));
   }, []);
 
   return { cartItems, loading };
