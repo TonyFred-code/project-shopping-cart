6a8e957bc90188490cb2b13864929206a264cc0f
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2025-11-28 17:16:22 +0100

    feat: add function for updating/uploading cart items

--- src/helpers/useCartItems.jsx
+++ src/helpers/useCartItems.jsx
@@ -1,9 +1,32 @@
 import { useEffect, useState } from 'react';
 import fetchCartItems from './fetchCartItems.js';
+import { CART_KEY } from '@/constants/cartsCache.js';
+
+function fruitInCart(fruitId, cartItems) {
+  return cartItems.some((cartItem) => cartItem.id === fruitId);
+}
 
 export default function useCartItems() {
   const [loading, setLoading] = useState(true);
   const [cartItems, setCartItems] = useState([]);
+
+  function uploadCartItem(quantity, fruitId) {
+    const isFruitInCart = fruitInCart(fruitId, cartItems);
+    let updatedCartItems = [];
+    if (isFruitInCart) {
+      updatedCartItems = cartItems.map((cartItem) => {
+        if (cartItem.id === fruitId) {
+          return { ...cartItem, quantity: cartItem.quantity + quantity };
+        }
+
+        return cartItem;
+      });
+    } else {
+      updatedCartItems = [...cartItems, { id: fruitId, quantity }];
+    }
+
+    setCartItems(updatedCartItems);
+  }
 
   useEffect(() => {
     fetchCartItems()
@@ -11,5 +34,5 @@ export default function useCartItems() {
       .finally(() => setLoading(false));
   }, []);
 
-  return { cartItems, loading };
+  return { cartItems, loading, uploadCartItem };
 }
